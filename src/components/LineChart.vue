69d571eafdeea687a42f0cb550df67529ce7b4b8
furkan ceylan <ceylan.furkan@outlook.com>
2021-05-15 04:03:48 +0300

    chartjs added

---
+++ src/components/LineChart.vue
@@ -0,0 +1,35 @@
+
+
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  props: {
+    label: {
+      type: String,
+    },
+    chartdata: {
+      type: String,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+  mounted() {  
+    const date = this.chartData.map(d => data[date]).reverse();
+    const totals = this.chartData.map(d => data[date].d).reverse();
+
+    this.renderChart({
+        labels: dates,
+        datasets: [{ 
+            label: this.label,
+            data: totals
+        }],
+    },
+    this.options);
+  },
+};
+</script>
